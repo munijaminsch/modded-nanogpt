f926bd76f6c41b893b5196a9e88224aa4f8eb575
Muni Jaminsch <munijaminsch@users.noreply.github.com>
2026-05-23 20:09:05 +0800

    Reduce default num_chunks to 103 (~10B tokens) and add download progress prints

--- data/cached_fineweb100B.py
+++ data/cached_fineweb100B.py
@@ -8,8 +8,11 @@ def get(fname):
     if not os.path.exists(os.path.join(local_dir, fname)):
         hf_hub_download(repo_id="kjj0/fineweb100B-gpt2", filename=fname,
                         repo_type="dataset", local_dir=local_dir)
+        print(f"Downloaded: {fname}")
+    else:
+        print(f"Already cached: {fname}")
 get("fineweb_val_%06d.bin" % 0)
-num_chunks = 1030 # full fineweb100B. Each chunk is 100M tokens
+num_chunks = 103 # reduced default: ~10B tokens instead of full 100B, sufficient for most experiments
 if len(sys.argv) >= 2: # we can pass an argument to download less
     num_chunks = int(sys.argv[1])
 for i in range(1, num_chunks+1):
